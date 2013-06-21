144afb1c2cb401a769cc798dd30626c3f7d84bf8
anthonydanger <anthony.sanchez.sf@gmail.com>
2013-06-21 15:06:01 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashier::Application.config.secret_token = 'd24465ac8c04199e4dc30ef753f8b3834aee085d27e5c9c0efd2af499f892863ae82b0c3a7a070dcbf41543494b18c1f8d72c22903422b9161016fd11144a090'
